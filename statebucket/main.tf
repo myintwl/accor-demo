df83bd863d047311e66e6cd0c4508b711d4f5fa4
myintwl <53091165+myintwl@users.noreply.github.com>
2026-03-05 11:52:15 +0700

    Add AWS provider and S3 bucket configuration

--- statebucket/main.tf
+++ statebucket/main.tf
@@ -1,6 +1,7 @@
 ###############################################################################
 # Provider
 ###############################################################################
+
 provider "aws" {
   region              = var.region
   allowed_account_ids = [var.aws_account_id]
@@ -18,6 +19,7 @@ terraform {
 ###############################################################################
 # S3 Bucket
 ###############################################################################
+
 resource "aws_s3_bucket" "state" {
   bucket        = "${var.aws_account_id}-bucket-state-file-karpenter"
   force_destroy = true
